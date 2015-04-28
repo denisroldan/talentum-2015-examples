21e947e5faabe61b7878f739b8e0fbf458e07603
Denis Roldán <denis.roldan.estebanez@gmail.com>
2015-04-28 20:00:49 +0200

    Created Game and Character models, added to admin site and linked to Installed apps. Uploaded initial migration schemas.

--- python/demo-django/code/character/models.py
+++ python/demo-django/code/character/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from game.models import Game
 
-# Create your models here.
+
+class Character(models.Model):
+    name = models.CharField(max_length=250)
+    game = models.ManyToManyField(Game, related_name='games')
+
+    def __unicode__(self):
+        return "{0}".format(self.name)
+
+    def __str__(self):
+        return "{0}".format(self.name)
