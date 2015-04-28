8c3be25633cbd6ac7e3697a0768bbac10732eb8f
Denis Roldán <denis.roldan.estebanez@gmail.com>
2015-04-28 19:55:15 +0200

    Added clean Django project

---
+++ python/demo-django/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/denisroldan/Projects/talentum-2015-examples/python/demo-django/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
